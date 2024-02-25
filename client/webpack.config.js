9cc17c2728b8fc289154bdbaee877655c5646dc2
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-25 18:34:47 -0500

    Added css loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,23 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        // Add css loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Add babel
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
