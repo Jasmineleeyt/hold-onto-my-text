1ecbcf785718682f2365b8867ad1c1f3fcc54ad0
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-26 01:12:20 -0500

    Added event listner to handle click event and the appinstalled event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,3 +10,25 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
+// Click event on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+     return;
+    }
+  
+    // Show prompt
+    promptEvent.prompt();
+    
+    // Reset the deferred prompt variable
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Handle the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
