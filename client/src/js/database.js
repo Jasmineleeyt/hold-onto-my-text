34dab88ffb445ebaa9ab24357cf5306296e592b8
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-26 01:00:24 -0500

    Added functionality to retrieve content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,4 +29,21 @@ export const putDb = async (content) => {
 
 } 
 
+// Gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  // Create a connection to the jate database and version we want to use
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction, specify the database posting to, and specify the data privilege to be readonly
+  const tx = jateDb.transaction('jate','readonly');
+  // Open up the object store
+  const store = tx.objectStore('jate');
+    // Use getall method to get all content
+  const request = store.getAll();
+  // Confirm the data is fetched
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
 initdb();
